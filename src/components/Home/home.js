253c9876b051895313d19bbb529aee1f62ff0fbb
Rolph Fadini <rolphfadini@hotmail.com>
2024-07-22 17:16:01 -0300

    Adding the animated letters in the home page welcome text

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,17 +1,59 @@
+import React, { useState, useEffect } from 'react'
+
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-r.png'
 import './home.scss'
+import AnimatedLetters from '../animated-letters/animated-letters'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['o', 'l', 'p', 'h']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+    const bounceAnimation = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+
+    return () => clearTimeout(bounceAnimation)
+  }, [])
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm
-          <img src={LogoTitle} alt="developer" />
-          olph
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
           <br />
-          web developer
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
         </h1>
         <h2>Frontend Developer</h2>
         <Link to="/contact" className="flat-button">
