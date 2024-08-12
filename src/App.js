3f74f57a401be64cd89276083fb497877782d78a
Rolph Fadini <rolphfadini@hotmail.com>
2024-08-12 18:02:14 -0300

    Adding 'projects' page to routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './components/Layout/layout'
 import Home from './components/Home/home'
 import About from './components/about/about'
 import Contact from './components/contact/contact'
+import Projects from './components/projects/projects'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
+          <Route path="projects" element={<Projects />} />
         </Route>
       </Routes>
     </>
