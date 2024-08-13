e6af0a9112f78b23fdc6f7f771bee599eb71445f
Rolph Fadini <rolphfadini@hotmail.com>
2024-08-13 17:02:09 -0300

    Creating the renderProjects method to render the projects mocked data

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import './projects.scss'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../animated-letters/animated-letters'
+import projectsData from '../../data/projects.json'
 
 const Projects = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -14,6 +15,34 @@ const Projects = () => {
     return () => clearTimeout(bounceAnimation)
   }, [])
 
+  const renderProjects = (projects) => {
+    return (
+      <div className="images-container">
+        {projects.map((project, i) => {
+          return (
+            <div className="image-box" key={i}>
+              <img
+                className="project-image"
+                src={project.cover}
+                alt="project"
+              />
+              <div className="content">
+                <h2 className="title">{project.title}</h2>
+                <h4 className="description">{project.description}</h4>
+                <button
+                  className="btn"
+                  onClick={() => window.open(project.url)}
+                >
+                  View
+                </button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
   return (
     <>
       <div className="container projects-page">
@@ -24,6 +53,7 @@ const Projects = () => {
             idx={15}
           />
         </h1>
+        <div>{renderProjects(projectsData.projects)}</div>
       </div>
       <Loader type="ball-pulse" />
     </>
