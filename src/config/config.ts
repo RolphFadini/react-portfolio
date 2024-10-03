baba116ac275e71294cf8b10785cc8ba81123b00
Rolph Fadini <rolphfadini@hotmail.com>
2024-10-03 16:56:13 -0300

    Creating config.ts to manage public keys

---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+const config = {
+  emailJsPublicKey: process.env.REACT_APP_EMAILJS_PUBLIC_KEY,
+  emailJsTemplateKey: process.env.REACT_APP_EMAILJS_TEMPLATE_KEY,
+  emailJsServiceKey: process.env.REACT_APP_EMAILJS_SERVICE_KEY,
+}
+
+export default config
